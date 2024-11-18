c1a72c68afc8e4b7f5f26f3543d1b856036522a4
apple <apple@apples-MacBook-Air.local>
2024-11-18 17:32:02 +0530

    initial commit

--- Database/db.js
+++ Database/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const uri = "mongodb+srv://ranjana:3esAY2PwscfcHAMk@glampingapi.ktx34.mongodb.net/SSO-test-Api?retryWrites=true&w=majority&appName=GlampingApi";
 
 const connectDb = () => {
-    console.log("connected database" , )
+    console.log("connected database" )
     return mongoose.connect(uri)
     
     
